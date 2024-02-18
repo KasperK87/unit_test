a91be96580fbc8bc7c32dd022463221cef131311
Kasper Kristensen <kkrist87@gmail.com>
2024-02-18 07:45:31 +0100

    added .class file to .gitignore

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class Test1 {
+public class CalculatorTest {
 
     private final Calculator calculator = new Calculator();
 
